a1cb8b7eeef54e31e5fec5af403be0679773bf63
sangjdev <sangjdev@gmail.com>
2018-05-23 23:52:29 +0900

    env gitignore path설정

--- sangj-backend/src/index .js
+++ sangj-backend/src/index .js
@@ -1,15 +1,19 @@
-require('dotenv').config();
-const {
-    PORT: port
-  } = process.env;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-app.get('/', function (req, res) {
+const path = require('path');
+const p = path.join(__dirname, '../.env');
 
-    res.send('hello world');
+require('dotenv').config({path: p});
+const {
+    PORT: port
+  } = process.env;
+
+app.get('/', function (req, res) {
+    console.log('p : ' + p );
+    res.send('hello worl23d');
 })
 
 app.listen(port, function () {
