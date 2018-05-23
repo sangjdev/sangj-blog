258ca8860a04a11979656fafa567a9b51a7820b8
sangjdev <sangjdev@gmail.com>
2018-05-23 23:45:22 +0900

    백엔드 config설정

--- sangj-backend/src/index .js
+++ sangj-backend/src/index .js
@@ -1,19 +1,18 @@
 require('dotenv').config();
-
+const {
+    PORT: port
+  } = process.env;
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const env = process.env;
 
 app.get('/', function (req, res) {
 
     res.send('hello world');
 })
 
-app.listen(env.port, function () {
-    console.log("env"+ JSON.stringify(env));
-    console.log('express server has started on port 3355');
+app.listen(port, function () {
+
     console.log("llgg" + process.env.PORT);
 });
